51e0c179149fe084fc6d115a9d651f56fea475a2
Paul Tierney <paul.tierney@outlook.ie>
2017-11-17 12:33:40 +0000

    Removed methods not being used

--- northwindRepo.js
+++ northwindRepo.js
@@ -9,36 +9,6 @@ northwindRepo.init = function (url) {
     northwindRepo.Url = url;
 };
 
-northwindRepo.getOrders = function (expand, select) {
-    return new Promise(function (fullfil, reject) {
-
-        var thisUrl = northwindRepo.Url.trim() + "/Orders" + "?$format=json"
-
-        thisUrl = buildOdataUrl(thisUrl, expand, select);
-
-        console.log("Querying Service " + thisUrl);
-        rest.get(thisUrl).then(function (data) {
-        
-            
-
-            fullfil(data.value);
-        }
-        ).catch(reject);
-    });
-};
-
-northwindRepo.getEmployees = function (expand, select) {
-    return new Promise(function (fullfil, reject) {
-        var thisUrl = northwindRepo.Url.trim() + "/Employees" + "?$format=json"
-        thisUrl = buildOdataUrl(thisUrl, expand, select);
-
-        console.log("Querying Service " + thisUrl);
-        rest.get(thisUrl).then(function (data) {
-            fullfil(data.value);
-        }
-        ).catch(reject);
-    });
-};
 
 northwindRepo.getTerritories = function (expand, select) {
     return new Promise(function (fullfil, reject) {
